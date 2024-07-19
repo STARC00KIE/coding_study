b332a4004f09e8ec85147effb988eba60d7c5af8
STARC00KIE <122951469+STARC00KIE@users.noreply.github.com>
2024-07-19 17:14:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 46.43 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,18 +1,20 @@
-"""
 def solution(participant, completion):
-    for comp in completion:
-        participant.remove(comp)
-        
-    answer = participant.pop(0)
-    return answer
-"""
+    dict = {}
+    
+    # 선수 이름 수로 정렬
+    for p in participant:
+        if p in dict:
+            dict[p] += 1
+        else:
+            dict[p] = 1
 
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    
-    for p, c in zip(participant, completion):
-        if p != c:
-            return p
-    
-    return participant[-1]
+    # 완주자 한명씩 제거
+    for c in completion:
+        if c in dict:
+            dict[c] -= 1
+            
+    # 미완주자 찾기
+    for key in dict.keys():
+        if dict[key] > 0:
+            return key
+        
