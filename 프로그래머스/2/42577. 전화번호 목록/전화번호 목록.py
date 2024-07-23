3983179a55a14defdf12d4dfa8a2de56ac8b961f
STARC00KIE <122951469+STARC00KIE@users.noreply.github.com>
2024-07-23 20:26:00 +0900

    [level 2] Title: 전화번호 목록, Time: 94.88 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -4,7 +4,7 @@
 
 def solution(phone_book):
     N = phone_book.pop(0)  # 구조대 번호
-    
+     
     for num in phone_book:
         if num.startswith(N):
             return False
@@ -14,6 +14,7 @@ def solution(phone_book):
 def solution(phone_book):
     phone_book.sort() # 정렬
     
+    # 한 번호가 다른 번호의 접두어인 경우 확인
     for i in range(len(phone_book) - 1):
         if phone_book[i+1].startswith(phone_book[i]):
             return False
