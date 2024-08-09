8aa98818fe29ee54429d63a2a9cbf8a1976172b8
STARC00KIE <122951469+STARC00KIE@users.noreply.github.com>
2024-08-09 20:03:04 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,15 @@
+def solution(cards1, cards2, goal):
+    while goal:
+        if cards1 and cards1[0] == goal[0]:
+            goal.pop(0)
+            cards1.pop(0)
+        elif cards2 and cards2[0] == goal[0]:
+            goal.pop(0)
+            cards2.pop(0)
+        else:
+            return 'No'
+    return 'Yes'
+                
+            
+        
+        
