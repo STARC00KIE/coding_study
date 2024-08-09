2d20d83df2689fd816ad37176abd10f25670ca7d
STARC00KIE <122951469+STARC00KIE@users.noreply.github.com>
2024-08-09 18:33:37 +0900

    [level 0] Title: 소인수분해, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,17 @@
+def solution(n):
+    if n <= 1:
+        return []
+
+    factors = []
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            factors.append(i)
+            while n % i == 0:
+                n = n // i
+        else:
+            i = i + 1
+    if n > 1:
+        factors.append(n)
+    
+    return factors
